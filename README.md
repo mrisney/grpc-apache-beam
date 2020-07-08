55a884d93e230399e92ccdda76a9a6aae72f7e01
Marc Risney <marcrisney@marcs-mbp.home>
2020-07-07 20:09:34 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# grpc-apache-beam
+### grpc-apache-beam
+
+### Simple GRPC Client Server
+### Prerequisites
+-------------
+This project uses Maven for assembling a jar filebased on artifacts in Maven Central or
+any other accessible Maven repository.
+
+1. Install Java (JDK 8 preferred) and Maven.
+2. Clone this repository and go into the repository root folder.
+3.  `cd grpc-apache-beam`
+
+###  How to build the GRPC Hello World Server 
+---------------------------------
+1. Clone this repository and go into the repository root folder.
+2. Run the following command to build a jar file with all depencies
+3.  `mvn clean install`
+4.  This will build a 'FAT' jar with all dependecies. 
+
+### Running the GRPC Server
+to run the server.
+```sh
+$ cd grpc-apache-beam
+$ java -jar target/grpc-beam-1.0-jar-with-dependencies.jar
+```
---
+++ src/main/java/com/deloitte/beam/grpc/server/HelloServiceImpl.java
@@ -0,0 +1,29 @@
+package com.deloitte.beam.grpc.server;
+
+import com.deloitte.beam.grpc.HelloRequest;
+import com.deloitte.beam.grpc.HelloResponse;
+import com.deloitte.beam.grpc.HelloServiceGrpc.HelloServiceImplBase;
+
+import io.grpc.stub.StreamObserver;
+
+public class HelloServiceImpl extends HelloServiceImplBase {
+
+    @Override
+    public void hello(
+      HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        System.out.println("Request received from client:\n" + request);
+
+        String greeting = new StringBuilder().append("Hello, ")
+            .append(request.getFirstName())
+            .append(" ")
+            .append(request.getLastName())
+            .toString();
+
+        HelloResponse response = HelloResponse.newBuilder()
+            .setGreeting(greeting)
+            .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
